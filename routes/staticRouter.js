5369bf056634d7e2c3b66941609b748e40cd6525
radoneje <53825282+radoneje@users.noreply.github.com>
2023-04-14 17:39:01 +0300

    ыы

--- routes/staticRouter.js
+++ routes/staticRouter.js
@@ -36,9 +36,14 @@ router.get('/image/:size/:id', async function (req, res, next) {
         let small=orig.replace("uploads","uploads/"+req.params.size)
         console.log(orig,small )
 
-        if(fs.existsSync(small))
+        if(fs.existsSync(small)) {
+            console.log("downlod from cache")
             return res.download(r[0].path, small)
+        }
+        console.log("try resize")
         let dir=__dirname+"/../public/uploads/"+req.params.size;
+
+
         if (!fs.existsSync(dir)){
             fs.mkdirSync(dir);
         }
