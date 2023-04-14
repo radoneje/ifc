7f5f62df4c7745dbd1094c8417f9f1ddfd8c1dba
radoneje <53825282+radoneje@users.noreply.github.com>
2023-04-14 22:58:22 +0300

    ыы

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -19,14 +19,14 @@ router.get('/registration/:lang?', async function(req, res, next) {
     return res.redirect("/registration/ru")
   if(!req.params.lang.match(/ru|en/))
     res.redirect("/registration/ru")
-  res.render('registration',{lang:req.params.lang, ru:req.params.lang=="ru", apiUrl:config.apiUrl} );
+  res.render('pageRegistration',{lang:req.params.lang, ru:req.params.lang=="ru", apiUrl:config.apiUrl} );
 });
 router.get('/smi/:lang?', async function(req, res, next) {
   if(!req.params.lang)
     return res.redirect("/smi/ru")
   if(!req.params.lang.match(/ru|en/))
     res.redirect("/smi/ru")
-  res.render('registration',{lang:req.params.lang, ru:req.params.lang=="ru", apiUrl:config.apiUrl, typeid:4} );
+  res.render('pageRegistration',{lang:req.params.lang, ru:req.params.lang=="ru", apiUrl:config.apiUrl, typeid:4} );
 });
 
 router.get('/news/:lang?', async function(req, res, next) {
@@ -35,9 +35,19 @@ router.get('/news/:lang?', async function(req, res, next) {
   if(!req.params.lang.match(/ru|en/))
     res.redirect("/news/ru")
 
-  let news=await req.knex("t_news").where({status:2}).orderBy("sort","desc")
+  let news=await req.knex("t_news").where({status:2, lang:req.params.lang}).orderBy("sort","desc")
 
-  res.render('news',{lang:req.params.lang, ru:req.params.lang=="ru", news} );
+  res.render('pageNews',{lang:req.params.lang, ru:req.params.lang=="ru", news} );
+});
+router.get('/speakers/:lang?', async function(req, res, next) {
+  if(!req.params.lang)
+    return res.redirect("/speakers/ru")
+  if(!req.params.lang.match(/ru|en/))
+    res.redirect("/speakers/ru")
+
+  let speakers=await req.knex("t_pgm_spk").where({isEnabled:true,}).orderBy("sort","desc").orderBy("f"+req.params.lang,"desc").orderBy("i"+req.params.lang,"desc")
+
+  res.render('pageSpeakers',{lang:req.params.lang, ru:req.params.lang=="ru", speakers} );
 });
 
 router.get('/photoEditor', async function(req, res, next) {
