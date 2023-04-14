c7cd54a67f3ec3a4d9800887e78060c10a093de7
radoneje <53825282+radoneje@users.noreply.github.com>
2023-04-14 17:10:27 +0300

    ыы

--- public/static/javascripts/utils.js
+++ public/static/javascripts/utils.js
@@ -122,39 +122,49 @@ const getPhoto = async (aspectRatio=4/5) => {
             var fr = new FileReader();
             fr.readAsDataURL(inp.files[0]);
             fr.onload = () => {
-                photoEditorImage.src = fr.result
+                let image = new Image();
+                image.src = fr.result;
+                image.onload=()=>{
+                    let coof=600/image.width;
+                    let canvas = document.createElement('canvas');
+                    canvas.width = coof*image.width;
+                    canvas.height = coof*image.height;
+                    canvas.getContext('2d').drawImage(image, 0, 0, canvas.width, canvas.height);
+                    photoEditorImage.src=canvas.toDataURL('image/jpeg');
+                    const cropper = new Cropper(photoEditorImage, {
+                        aspectRatio: aspectRatio,//1,//    9/16
+                        viewMode: 1,
+                        autoCropArea: 1,
+                        zoomable: false,
+                    });
+                    photoSaveBtn.onclick = () => {
+                        if(photoSaveBtn.classList.contains("loading"))
+                            return;
+                        let txt=photoSaveBtn.innerHTML;
 
-                const cropper = new Cropper(photoEditorImage, {
-                    aspectRatio: aspectRatio,//1,//    9/16
-                    viewMode: 1,
-                    autoCropArea: 1,
-                    zoomable: false,
-                });
-                photoSaveBtn.onclick = () => {
-                    if(photoSaveBtn.classList.contains("loading"))
-                        return;
-                    let txt=photoSaveBtn.innerHTML;
-                    alert("1.2"+txt);
-                    photoSaveBtn.innerHTML="сохраняю..."
-                    photoSaveBtn.classList.add("loading")
-                    cropper.getCroppedCanvas().toBlob(async (blob) => {
-                        let formData = new FormData()
-                        formData.append('file', blob, 'userPhoto.png');
-                        alert(1.1);
+                        photoSaveBtn.innerHTML="сохраняю..."
+                        photoSaveBtn.classList.add("loading")
+                        cropper.getCroppedCanvas().toBlob(async (blob) => {
+                            let formData = new FormData()
+                            formData.append('file', blob, 'userPhoto.png');
+                            alert(1.1);
 
-                        let ret = await fetch(frontUrl + "/api/uploadFile", {
-                            method: 'post',
-                            body: formData,
-                        })
+                            let ret = await fetch(frontUrl + "/api/uploadFile", {
+                                method: 'post',
+                                body: formData,
+                            })
 
-                        if (ret.ok)
-                            responce(await ret.json())
-                        else
-                            alert(3);
+                            if (ret.ok)
+                                responce(await ret.json())
+                            else
+                                alert(3);
 
-                        document.body.removeChild(elem);
-                    }, 'image/png', 1)
+                            document.body.removeChild(elem);
+                        }, 'image/png', 1)
+                    }
+
                 }
+
             }
         }
     })
@@ -204,5 +214,28 @@ const validateEmail = (email) => {
             /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
         );
 };
+var dataURLToBlob = function(dataURL) {
+    var BASE64_MARKER = ';base64,';
+    if (dataURL.indexOf(BASE64_MARKER) == -1) {
+        var parts = dataURL.split(',');
+        var contentType = parts[0].split(':')[1];
+        var raw = parts[1];
+
+        return new Blob([raw], {type: contentType});
+    }
+
+    var parts = dataURL.split(BASE64_MARKER);
+    var contentType = parts[0].split(':')[1];
+    var raw = window.atob(parts[1]);
+    var rawLength = raw.length;
+
+    var uInt8Array = new Uint8Array(rawLength);
+
+    for (var i = 0; i < rawLength; ++i) {
+        uInt8Array[i] = raw.charCodeAt(i);
+    }
+
+    return new Blob([uInt8Array], {type: contentType});
+}
 
 
