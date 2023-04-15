1aeb9cf4442eb6a2ae09b6c3a3aad62fad86d537
radoneje <53825282+radoneje@users.noreply.github.com>
2023-04-16 01:23:28 +0300

    ыы

--- routes/staticRouter.js
+++ routes/staticRouter.js
@@ -33,6 +33,7 @@ router.get('/key/:key', async function (req, res, next) {
     try {
         console.log("-."+req.params.key+".-")
         let r = await req.knex("t_staticfiles").where({key: req.params.key})
+        console.log("-."+req.params.key+".-", r)
         if (r.length==0)
             return res.sendStatus(404)
         let url="/static/file/"+r[0].fileid
