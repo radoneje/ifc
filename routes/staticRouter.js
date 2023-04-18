f27ddd20c762f3c09bc0deb779ec2b5f6e7a9800
radoneje <53825282+radoneje@users.noreply.github.com>
2023-04-18 14:49:14 +0300

    ыы

--- routes/staticRouter.js
+++ routes/staticRouter.js
@@ -106,7 +106,7 @@ router.get('/invoice/:guid', async function (req, res, next) {
                 .font("/var/fonts/Arial.ttf")///var/fonts/OpenSans-Regular-2.ttf")
                 .fontSize(12)
                 .fillColor('#000000')
-                .text("22 от 22 02 2009г.", 200, 100,{width: 400})
+                .text("22 от 22 02 2009г.", 300, 200,{width: 400})
             doc.addPage()
                 .image(__dirname+"/../forpdf/invoice/02.png",0,0,{width:600})
             doc.addPage()
