3ff88d3b6f99c415f4aafabc12a94edcddec2cab
radoneje <53825282+radoneje@users.noreply.github.com>
2023-05-15 22:07:50 +0300

    ыы j

--- public/static/javascripts/mainScript.js
+++ public/static/javascripts/mainScript.js
@@ -376,7 +376,7 @@ try {
 function scroolRiht(ctrl){
     let track=ctrl.parentNode.parentNode.querySelector(".oTrack")
     let sessWr=track.querySelector(".oSessionScroll")
-    let width=window.innerWidth-64;
+    let width=window.innerWidth-32;
 
     console.log(track.scrollLeft, sessWr.scrollLeft)
     sessWr.scrollLeft+=width;
