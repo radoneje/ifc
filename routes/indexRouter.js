d643d36ede6001d8d589a17da2baa47cf743034d
radoneje <53825282+radoneje@users.noreply.github.com>
2023-05-21 00:20:21 +0300

    ыы j

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -97,6 +97,7 @@ router.get('/programme/:lang?', async function(req, res, next) {
     if (!req.params.lang.match(/ru|en/))
       res.redirect("/programme/ru")
     let pgm = await req.knex("v_pgm")
+    console.log(pgm)
     res.render('pageProgramme', {lang: req.params.lang, ru: req.params.lang == "ru", pgm});
   }
   catch (e) {
