52bb9a4be7c10fa8a2786c450323969d67610fd4
radoneje <53825282+radoneje@users.noreply.github.com>
2023-05-21 20:12:03 +0300

    ыы j

--- routes/personalRouter.js
+++ routes/personalRouter.js
@@ -270,7 +270,7 @@ router.get('/info/:lang?', checkAccess, async function(req, res, next) {
             req.params.lang="ru";
 
         let hotels=await req.knex("v_hotels").where({isEnabled:true}).orderBy("stars").orderBy("nameru")
-        res.render("personal/layout", {lang:req.params.lang,ru: req.params.lang == "ru", apiUrl:config.apiUrl, hotels}, )
+        res.render("personal/layout", {lang:req.params.lang, ru:req.params.lang == "ru", apiUrl:config.apiUrl, hotels}, )
 
     }
     catch (e) {
