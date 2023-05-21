12d4d804097d54c38c6a3d195e2c044ef5bcfe3f
radoneje <53825282+radoneje@users.noreply.github.com>
2023-05-21 23:00:06 +0300

    ыы j

--- routes/staticRouter.js
+++ routes/staticRouter.js
@@ -347,7 +347,7 @@ router.get('/akt/:guid', async function (req, res, next) {
         let inv=invoices[0]
             //return res.json(inv)
 
-        let filename="/var/ifc_data/acts/act_"+String(inv.id).padStart(3, '0')+String(Math.random())+".pdf"
+        let filename="/var/ifc_data/acts/act_"+String(inv.id).padStart(3, '0')+String(moment().unix())+".pdf"
         if (fs.existsSync(filename)) {
             //return res.download(filename);
             fs.rmSync(filename)
