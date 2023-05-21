022861e155b3f19c38c2866d2a54f0441d3796b3
radoneje <53825282+radoneje@users.noreply.github.com>
2023-05-21 23:15:17 +0300

    ыы j

--- routes/staticRouter.js
+++ routes/staticRouter.js
@@ -387,6 +387,7 @@ router.get('/akt/:guid', async function (req, res, next) {
             .text( "ЗАЯВКА № "+inv.user[0].id+"от "+ moment(inv.user[0].date).format("DD.MM.YYYY г."), /*x*/ 108 , /*y*/ 272,{width: 400})
             .fontSize(8)
             .text( price+" 000.00", /*x*/ 450 , /*y*/ 321,{width: 200})
+            .text( price+" 000.00", /*x*/ 450 , /*y*/ 380,{width: 200})
             .text( price+" 000.00 рублей" , /*x*/ 210 , /*y*/ 415,{width: 200})
             .text( pricetxt+" тысяч рублей 00 копеек, НДС не облагается (ст 346.12 и 346.13 гл. 26.2 НК РФ)", /*x*/ 45 , /*y*/ 426,{width: 600})
         doc.end();
