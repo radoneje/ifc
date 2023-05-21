06489b2f49f63c5ff9cc157b495c9cba826be986
radoneje <53825282+radoneje@users.noreply.github.com>
2023-05-21 22:51:11 +0300

    ыы j

--- routes/staticRouter.js
+++ routes/staticRouter.js
@@ -382,7 +382,7 @@ router.get('/akt/:guid', async function (req, res, next) {
             .fillColor('#000000')
             .text( "ФК-"+inv.id, /*x*/ 95 , /*y*/ 122,{width: 400})
             .fontSize(10)
-            .text( recvizit, /*x*/ 100 , /*y*/ 230,{width: 400})
+            .text( recvizit, /*x*/ 110 , /*y*/ 220,{width: 400})
         doc.end();
         setTimeout(()=>{res.download(filename)},1000)
 
