cbd3e3e07aa26be5f557b4c711a100fb86c4d63f
radoneje <53825282+radoneje@users.noreply.github.com>
2023-05-21 23:12:35 +0300

    ыы j

--- routes/staticRouter.js
+++ routes/staticRouter.js
@@ -388,7 +388,7 @@ router.get('/akt/:guid', async function (req, res, next) {
             .fontSize(8)
             .text( price+" 000.00", /*x*/ 450 , /*y*/ 321,{width: 200})
             .text( price+" 000.00 рублей" , /*x*/ 210 , /*y*/ 415,{width: 200})
-            .text( pricetxt+" тысяч рублей 00 копеек,\nНДС не облагается (ст 346.12 и 346.13 гл. 26.2 НК РФ)", /*x*/ 40 , /*y*/ 450,{width: 600})
+            .text( pricetxt+" тысяч рублей 00 копеек,\nНДС не облагается (ст 346.12 и 346.13 гл. 26.2 НК РФ)", /*x*/ 45 , /*y*/ 435,{width: 600})
         doc.end();
         setTimeout(()=>{res.download(filename)},1000)
 
