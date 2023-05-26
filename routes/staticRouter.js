b59e75829e0ac437d9c485d58899a68144734633
radoneje <53825282+radoneje@users.noreply.github.com>
2023-05-26 10:27:08 +0300

    ыы j

--- routes/staticRouter.js
+++ routes/staticRouter.js
@@ -256,7 +256,7 @@ router.get('/invoice/:guid', async function (req, res, next) {
             let recvizit=inv.company[0].name+","
             recvizit+="\nИНН "+inv.company[0].inn+", КПП "+inv.company[0].kpp+","
             recvizit+="\n"+inv.company[0].address
-            if(inv.isPaySelf) {
+            if(inv.user[0].isPaySelf) {
                 recvizit = inv.user[0].f + " " + inv.user[0].i + " "+ inv.user[0].o
                 recvizit += "\nпаспорт:" +(inv.user[0].passportSerial || "")+" "+ inv.user[0].passportNumber +", выдан: "+ inv.user[0].passportDate+", код подразделения "+ inv.user[0].passportCode
             }
