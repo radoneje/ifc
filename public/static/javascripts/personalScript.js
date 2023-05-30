22a347fb8bdc42a8c0d56ccc96c99165ba9f6b73
radoneje <53825282+radoneje@users.noreply.github.com>
2023-05-30 12:52:15 +0300

    ыы j

--- public/static/javascripts/personalScript.js
+++ public/static/javascripts/personalScript.js
@@ -34,7 +34,7 @@ let personalApp = new Vue({
                   let name=box.querySelector(".coleguesRowName").innerText;
                   arr.push({userid, name})
               })
-              this.badgeDelivery.collegues.push(arr);
+              this.badgeDelivery.collegues=(arr);
               closePopUp();
           }
         },
