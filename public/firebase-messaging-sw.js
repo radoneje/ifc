c155a936d339b237ebda3b9ce0b8eda9317bc0d1
radoneje <53825282+radoneje@users.noreply.github.com>
2023-06-12 22:33:04 +0300

    ыы j

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -15,4 +15,8 @@ firebase.initializeApp({
 
 
 const messaging = firebase.messaging();
+messaging.onMessage(function(payload) {
+    console.log('Message received. ', payload);
+    new Notification(payload.notification.title, payload.notification);
+});
 
