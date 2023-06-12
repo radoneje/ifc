dd043b6ac472d9a6d43ccdb3a0b7e5f900ebee41
radoneje <53825282+radoneje@users.noreply.github.com>
2023-06-12 22:47:18 +0300

    ыы j

--- public/static/javascripts/personalScript.js
+++ public/static/javascripts/personalScript.js
@@ -429,6 +429,32 @@ let personalApp = new Vue({
 })
 
 if ('Notification' in window) {
+    firebase.initializeApp({
+        //messagingSenderId: '970751093432'
+        apiKey: "AIzaSyBZm4LHkOHWkolMuh2gtp24K072vQ5P7mI",
+        authDomain: "ifcongress-56476.firebaseapp.com",
+        projectId: "ifcongress-56476",
+        storageBucket: "ifcongress-56476.appspot.com",
+        messagingSenderId: "970751093432",
+        appId: "1:970751093432:web:2b6ac862eb43878007e24d",
+        measurementId: "G-DEWMH500G8"
+    });
+
+
+
+    const messaging = firebase.messaging();
+    messaging.onBackgroundMessage((payload) => {
+        console.log('[firebase-messaging-sw.js] Received background message ', payload);
+        const notificationTitle = payload.notification.title;
+        const notificationOptions = {
+            body: payload.notification.body,
+        };
+        return self.registration.showNotification(notificationTitle,
+            notificationOptions);
+    });
+    self.addEventListener('notificationclick', event => {
+        console.log(event)
+    });
     var messaging = firebase.messaging();
 
     messaging.onMessage(function(payload) {
