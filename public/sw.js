7114ffa2ffa850b2b2ad4296fbeddbdf70826031
radoneje <53825282+radoneje@users.noreply.github.com>
2023-06-12 17:07:36 +0300

    ыы j

---
+++ public/sw.js
@@ -0,0 +1,5 @@
+'use strict';
+importScripts('sw-toolbox.js');
+toolbox.precache(['/ru','/stylesheets/style.css']);
+toolbox.router.get('/images/*', toolbox.cacheFirst);
+toolbox.router.get('/*', toolbox.networkFirst, {networkTimeoutSeconds: 5});
