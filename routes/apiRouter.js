3daf936a80deb59b8dfb1331699568f70c00467c
radoneje <53825282+radoneje@users.noreply.github.com>
2023-06-12 12:26:01 +0300

    ыы j

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -30,7 +30,7 @@ router.get('/ping',   async function (req, res, next) {
     res.json("pong")
 });
 
-router.post('/sendQFromTGbot/', async function(req, res, next) {
+router.post('/sendQFromTGbot', async function(req, res, next) {
     try {
         let r = await req.knex("t_pgm_q_frombot").insert(req.body)
         res.json(true)
