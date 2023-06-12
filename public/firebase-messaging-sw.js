7b09f82b38191475102a1577c72f4b45d223053d
radoneje <53825282+radoneje@users.noreply.github.com>
2023-06-12 22:29:49 +0300

    ыы j

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -2,9 +2,15 @@ importScripts('https://www.gstatic.com/firebasejs/3.6.8/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/3.6.8/firebase-messaging.js');
 
 firebase.initializeApp({
-    messagingSenderId: '970751093432'
+    //messagingSenderId: '970751093432'
+    apiKey: "AIzaSyBZm4LHkOHWkolMuh2gtp24K072vQ5P7mI",
+    authDomain: "ifcongress-56476.firebaseapp.com",
+    projectId: "ifcongress-56476",
+    storageBucket: "ifcongress-56476.appspot.com",
+    messagingSenderId: "970751093432",
+    appId: "1:970751093432:web:2b6ac862eb43878007e24d",
+    measurementId: "G-DEWMH500G8"
 });
-
 
 
 
