9d3050dfda6a64c89384cc33fb328cb17310bc41
radoneje <53825282+radoneje@users.noreply.github.com>
2023-06-13 00:17:28 +0300

    ыы j

--- public/test_sw.js
+++ public/test_sw.js
@@ -1,5 +1,17 @@
+importScripts('https://www.gstatic.com/firebasejs/8.1.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.1.1/firebase-messaging.js');
+var firebaseConfig = {
+    apiKey: "AIzaSyBZm4LHkOHWkolMuh2gtp24K072vQ5P7mI",
+    authDomain: "ifcongress-56476.firebaseapp.com",
+    projectId: "ifcongress-56476",
+    storageBucket: "ifcongress-56476.appspot.com",
+    messagingSenderId: "970751093432",
+    appId: "1:970751093432:web:2b6ac862eb43878007e24d",
+    measurementId: "G-DEWMH500G8"
+};
+
 function t(){
-    console.log("t")
+    console.log("t2")
     setTimeout(t, 2000)
 }
 t();
