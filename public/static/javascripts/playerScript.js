a7738e95979c2d70e5c871c465db8fbf44c4c577
radoneje <53825282+radoneje@users.noreply.github.com>
2023-06-22 14:47:18 +0300

    ыы j

--- public/static/javascripts/playerScript.js
+++ public/static/javascripts/playerScript.js
@@ -5,6 +5,9 @@ let playerApp=new Vue({
     methods:{},
     mounted:async function(){
         console.log("playerScript")
+        let loader=document.querySelector(".playerLoader")
+        loader.parentNode.removeChild(loader)
+        document.querySelector(".playerBox").style.display="block"
     }
 })
 
