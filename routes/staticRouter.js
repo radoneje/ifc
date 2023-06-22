637137ce996c28c3e355d32c7c22ef3808c220e8
radoneje <53825282+radoneje@users.noreply.github.com>
2023-06-23 00:02:13 +0300

    ыы j

--- routes/staticRouter.js
+++ routes/staticRouter.js
@@ -443,7 +443,7 @@ router.get('/ticket/:userid', async function (req, res, next) {
     doc.pipe(fs.createWriteStream(filename));
     doc
         .image(__dirname+"/../forpdf/ticket_ru.png",0,0,{width:4500/4})
-        .image(QRfilename,800,1500, {width:(4500/4)/4})
+        .image(QRfilename,800,1800, {width:(4500/4)/4})
     doc.end();
     setTimeout(()=>{res.download(filename)},1000)
 
