fa397eaad96c50799eafe5acf0b9ad17086071bc
radoneje <53825282+radoneje@users.noreply.github.com>
2023-06-23 17:14:44 +0300

    ыы j

--- public/static/javascripts/playerScript.js
+++ public/static/javascripts/playerScript.js
@@ -1,54 +1,55 @@
 "use strict";
 
 function initPlayer() {
-    if(document.querySelector("#playerWindow"))
-    let playerApp = new Vue({
-        el: "#playerWindow",
-        data: {
-            status: {},
-            halls: [],
-            hallsUpdateTime: 0,
-            activeHall: null,
-        },
-        methods: {
-            updateLiveStatus: async function () {
-                let dt = await getJson("/liveStatus")
-                if (dt.liveStatus.updateTime != this.status.updateTime)
-                    this.status = (dt.liveStatus);
-                let maxTime = 0;
-                dt.hallStatus.forEach(h => {
-                    maxTime = Math.max(h.updateTime, maxTime)
-                })
-                if (dt.hallStatus.length > 0)
-                    if (this.halls.length != dt.hallStatus.length || maxTime != this.hallsUpdateTime) {
-                        this.hallsUpdateTime = maxTime
-                        this.halls = dt.hallStatus;
-                        if (!this.activeHall)
-                            this.activeHall = this.halls[0]
-                        if (this.halls.filter(h => h.id == this.activeHall.id).length == 0)
-                            this.activeHall = this.halls[0]
+    if (document.querySelector("#playerWindow")) {
+        let playerApp = new Vue({
+            el: "#playerWindow",
+            data: {
+                status: {},
+                halls: [],
+                hallsUpdateTime: 0,
+                activeHall: null,
+            },
+            methods: {
+                updateLiveStatus: async function () {
+                    let dt = await getJson("/liveStatus")
+                    if (dt.liveStatus.updateTime != this.status.updateTime)
+                        this.status = (dt.liveStatus);
+                    let maxTime = 0;
+                    dt.hallStatus.forEach(h => {
+                        maxTime = Math.max(h.updateTime, maxTime)
+                    })
+                    if (dt.hallStatus.length > 0)
+                        if (this.halls.length != dt.hallStatus.length || maxTime != this.hallsUpdateTime) {
+                            this.hallsUpdateTime = maxTime
+                            this.halls = dt.hallStatus;
+                            if (!this.activeHall)
+                                this.activeHall = this.halls[0]
+                            if (this.halls.filter(h => h.id == this.activeHall.id).length == 0)
+                                this.activeHall = this.halls[0]
 
-                        console.log("update halls")
-                    }
+                            console.log("update halls")
+                        }
+                    setTimeout(() => {
+                        this.updateLiveStatus();
+                    }, 10000)
+
+                }
+            },
+            mounted: async function () {
+
+                this.updateLiveStatus();
                 setTimeout(() => {
-                    this.updateLiveStatus();
-                }, 10000)
+                    let loader = document.querySelector("#playerLoader")
+                    if (loader) {
+                        loader.parentNode.removeChild(loader)
+                        document.querySelector("#playerBox").style.display = "block"
+                    }
+                }, 2000)
 
             }
-        },
-        mounted: async function () {
-
-            this.updateLiveStatus();
-            setTimeout(() => {
-                let loader = document.querySelector("#playerLoader")
-                if(loader) {
-                    loader.parentNode.removeChild(loader)
-                    document.querySelector("#playerBox").style.display = "block"
-                }
-            }, 2000)
-
-        }
-    })
+        })
+    }
 }
 
 initPlayer();
