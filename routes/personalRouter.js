d81d65b8480268d8904f78e7a946095a9619e239
radoneje <53825282+radoneje@users.noreply.github.com>
2023-07-13 11:52:20 +0300

    ыы j

--- routes/personalRouter.js
+++ routes/personalRouter.js
@@ -410,7 +410,7 @@ router.get('/data', async function(req, res, next) {
 });
 router.get('/info/:lang?', checkAccess, async function(req, res, next) {
     //return res.json(req.session.token)
-    //return res.sendStatus(404)
+    return res.sendStatus(404)
 
     try {
         if(!req.params.lang.match(/ru|en/))
