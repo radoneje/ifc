da310b88541dfc8d6a30f17531a469a1ec8ef0ed
radoneje <53825282+radoneje@users.noreply.github.com>
2023-07-13 11:51:49 +0300

    ыы j

--- routes/personalRouter.js
+++ routes/personalRouter.js
@@ -409,7 +409,7 @@ router.get('/data', async function(req, res, next) {
     }
 });
 router.get('/info/:lang?', checkAccess, async function(req, res, next) {
-    return res.json(req.session.token)
+    //return res.json(req.session.token)
     //return res.sendStatus(404)
 
     try {
